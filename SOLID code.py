0051f1fa7470ef151e88434ff56d2ce8047ead11
akhil118 <gandikota.abai@gmail.com>
2022-06-13 12:46:54 +0530

    code modified according to data

--- SOLID code.py
+++ SOLID code.py
@@ -1,35 +1,38 @@
 import csv
 
 
-def rawData(filename):
-    with open(filename, "r") as file:
-        csvReader = csv.DictReader(file)
-        data = list(csvReader)
-    return data
+class DataExtractor():
+
+    def rawData(filename):
+        with open(filename, "r") as file:
+            csvReader = csv.DictReader(file)
+            data = list(csvReader)
+        return data
 
 
-def result(CSVfile, max, min, name):
-    data = rawData(CSVfile)
-    # print(data)
+class DataAnalyzer():
 
-    result = ""
-    count = float(data[0][max]) - float(data[0][min])
+    def getResult(CSVfile, maximumCol, minimumCol, nameCol):
+        data = DataExtractor.rawData(CSVfile)
 
-    for eachItem in data:
-        difference = abs(float(eachItem[max]) - float(eachItem[min]))
-        if difference <= count:
-            count = difference
-            result = eachItem[name]
-            # print(count, result)
-    return(result)
+        result = ""
+        count = float(data[0][maximumCol]) - float(data[0][minimumCol])
+
+        for eachItem in data:
+            difference = abs(float(eachItem[maximumCol]) - float(eachItem[minimumCol]))
+            if difference <= count:
+                count = difference
+                result = eachItem[nameCol]
+                # print(count, result)
+        return(result)
 
 
 def main():
     weatherCSV = "/home/akhil118/Desktop/git/SOLID-project/weather.csv"
     footballCSV = "/home/akhil118/Desktop/git/SOLID-project/football.csv"
 
-    smallestTemperatureSpread = result(weatherCSV, "Max", "Min", "Day")
-    nameOfTheTeamWithTheSmallestDifference = result(footballCSV, "F", "A", "Team")
+    smallestTemperatureSpread = DataAnalyzer.getResult(weatherCSV, "Max", "Min", "Day")
+    nameOfTheTeamWithTheSmallestDifference = DataAnalyzer.getResult(footballCSV, "F", "A", "Team")
 
     print("The day number with the smallest temperature spread is day", smallestTemperatureSpread)
     print("Name of the team with the smallest difference is ", nameOfTheTeamWithTheSmallestDifference)
